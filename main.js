37068031b3d9623838b11436a1cbe4d802ac849f
Joseph Montesano <jmmmontesano@gmail.com>
2019-10-27 11:55:33 -0400

    updated ui

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("navigation");
+    console.log("i am working friend");
+    if (x.style.display === "none") {
+      x.style.display = "inline";
+    } else {
+      x.style.display = "none";
+    }
+  } 
