1bd1e3c608d7e5cd0b58d64f487a24fb7213e6f7
Joseph Montesano <jmmmontesano@gmail.com>
2019-11-04 11:03:16 -0500

    added hamburger icon on mobile/tablet, various minor ui and bugfixes

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-function myFunction() {
-    var x = document.getElementById("navigation");
-    console.log("i am working friend");
-    if (x.style.display === "none") {
-      x.style.display = "inline";
-    } else {
+function mobileNav() {
+  event.preventDefault();  
+  let x = document.getElementById('navContent');
+    console.log("i have been clicked lad");
+    console.log(x);
+    if (x.style.display === "inline"){
       x.style.display = "none";
     }
+    else{
+      console.log("we have changed the display to inline");
+      x.style.display = "inline";
+    }
+  
+    if (matchMedia) {
+      const mq = window.matchMedia("(min-width: 1080px)");
+      mq.addListener(WidthChange);
+    }  
   } 
+
+
+  function WidthChange(mq){
+    let x = document.getElementById('navContent');
+        if (mq.matches) {
+            x.style.display = "inline";
+            console.log("i should be working");
+        }
+}
+ 
