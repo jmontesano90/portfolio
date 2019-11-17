fd3f9f06b899227309c58f831da01a9904ebba25
Joseph Montesano <jmmmontesano@gmail.com>
2019-11-17 11:52:52 -0500

    removed console logs,added favicon, added links to navigate within page

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 function mobileNav() {
   event.preventDefault();  
   let x = document.getElementById('navContent');
-    console.log("i have been clicked lad");
     console.log(x);
     if (x.style.display === "inline"){
       x.style.display = "none";
     }
     else{
-      console.log("we have changed the display to inline");
       x.style.display = "inline";
     }
   
@@ -22,7 +20,6 @@ function mobileNav() {
     let x = document.getElementById('navContent');
         if (mq.matches) {
             x.style.display = "inline";
-            console.log("i should be working");
         }
 }
  
